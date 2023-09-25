3bbf73afc6af0573ed97209107729f92d5b155cd
Roberto Alves Martins <79550864+Robertogithu@users.noreply.github.com>
2023-09-25 00:30:37 -0300

    Add files via upload

---
+++ nodeJs/src/index.js
@@ -0,0 +1,28 @@
+import fs  from 'fs';
+import chalk from 'chalk';
+
+
+function extraiLinks(texto){
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: [captura[2]]}))
+    return resultados.length !== 0 ? resultados : 'não há links no arquivo';
+}
+
+function trataErro(erro) {
+    console.log(erro)
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+async function pegaArquivo(camionhoDoArquivo){
+    try{
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile
+    (camionhoDoArquivo, encoding)
+    return extraiLinks(texto);   
+    } catch(erro){
+        trataErro(erro)
+    }
+}
+
+export default pegaArquivo;
